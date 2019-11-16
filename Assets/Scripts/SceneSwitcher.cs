c0c9475aea4e075addadddfbe1d0060cbeafa3f6
Audrey Roy Greenfeld <aroy@alum.mit.edu>
2019-11-15 19:00:08 -0800

    Added new scene switcher

--- Assets/Scripts/SceneSwitcher.cs
+++ Assets/Scripts/SceneSwitcher.cs
@@ -5,20 +5,25 @@ using UnityEngine;
 public class SceneSwitcher : MonoBehaviour
 {
 
-    public void GoTo3dScanHome()
+    public void GoTo3dHomeScanner()
     {
         Debug.Log("Going to 3d scan of home");
     }
 
-    public void GoToReviewPlan()
+    public void GoToChecklists()
     {
         Debug.Log("Going to review of plan");
     }
 
+    public void GoToHome()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Home");
+    }
+
     public void GoToSmokeDetectorPlacement()
     {
-        Debug.Log("Going to smoke detector placement");
-        //UnityEngine.SceneManagement.SceneManager.LoadScene("SmokeDetectorPlacement");
+        //Debug.Log("Going to smoke detector placement");
+        UnityEngine.SceneManagement.SceneManager.LoadScene("SmokeDetectorList");
     }
 
 
