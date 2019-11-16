88f35a001dcef5a0d8fa58e67a6ee778634fd1d4
Daniel Roy Greenfeld <pydanny@gmail.com>
2019-11-15 16:44:48 -0800

    Add various assets

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneSwitcher : MonoBehaviour
+{
+
+    public void GoTo3dScanHome()
+    {
+        Debug.Log("Going to 3d scan of home");
+    }
+
+    public void GoToReviewPlan()
+    {
+        Debug.Log("Going to review of plan");
+    }
+
+    public void GoToSmokeDetectorPlacement()
+    {
+        Debug.Log("Going to smoke detector placement");
+        //UnityEngine.SceneManagement.SceneManager.LoadScene("SmokeDetectorPlacement");
+    }
+
+
+    public void GoToSmokeDetectorTestScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("SmokeDetectorTestScene");
+    }
+
+    
+
+}
